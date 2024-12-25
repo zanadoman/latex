03c11c1db4be20749a3b73ecefff3f121713068d
zanadoman <domanseven@gmail.com>
2024-12-25 17:12:04 +0100

    created example environment

--- reverse.c
+++ reverse.c
@@ -1,12 +1,7 @@
-/**
- * \brief Reverses a zero terminated string.
- * \param source Zero terminated string.
- * \param destination Output buffer for the reversed string.
- * \return Address of the last character in the output buffer.
- */
-char *reverse(const char *source, char *destination) {
+// Reverse a string with recursion
+char *rstrrev(const char *source, char *destination) {
     if (*source) {
-        destination = reverse(source + 1, destination);
+        destination = rstrrev(source + 1, destination);
         *destination++ = *source;
     }
     return destination;
